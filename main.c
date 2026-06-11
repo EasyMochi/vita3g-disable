5b2d25a25addb6bcd5ef69f146e79f7fe4178835
EasyMochi <7991344+EasyMochi@users.noreply.github.com>
2026-06-11 16:19:08 +0100

    Cleaned up main.c

--- main.c
+++ main.c
@@ -1,37 +1,23 @@
 #include <psp2/kernel/modulemgr.h>
 #include <taihen.h>
-#include <vitasdk.h>
 
-static SceUID hook;
+static SceUID hook = -1;
 static tai_hook_ref_t ref_hook;
-int vshSysconReturnTrue()
-{
-  return 0x01;
-}
-int vshSysconReturnFalse()
-{
-  return 0x00;
+
+static int vshSysconReturnFalse(void) {
+  return 0;
 }
 
-void _start() __attribute__((weak, alias("module_start")));
-
-int module_start(SceSize argc, const void *args)
-{
-
-  hook = taiHookFunctionImport(&ref_hook,
-                               TAI_MAIN_MODULE,
-                               TAI_ANY_LIBRARY,
-                               0xCEC8B917, // vshSysconHasWWAN
-                               vshSysconReturnFalse);
-
+int module_start(SceSize argc __attribute__((unused)), const void *args __attribute__((unused))) {
+  hook = taiHookFunctionImport(&ref_hook, TAI_MAIN_MODULE, TAI_ANY_LIBRARY, 0xCEC8B917, vshSysconReturnFalse);
   return SCE_KERNEL_START_SUCCESS;
 }
 
-int module_stop(SceSize argc, const void *args)
-{
-
-  if (hook >= 0)
+int module_stop(SceSize argc __attribute__((unused)), const void *args __attribute__((unused))) {
+  if (hook >= 0) {
     taiHookRelease(hook, ref_hook);
+    hook = -1;
+  }
 
   return SCE_KERNEL_STOP_SUCCESS;
 }
